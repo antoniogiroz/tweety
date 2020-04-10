009581705ccd62f6a5397573f3ba2a05a0355c46
Antonio Gil <antonioluisgil@gmail.com>
2020-04-10 23:54:15 +0200

    Design the timeline

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,12 @@
+<div class="px-8 py-6 mb-8 border border-blue-300 rounded-lg">
+    <form action="">
+        <textarea name="body" class="w-full" placeholder="What's up doc?"></textarea>
+
+        <hr class="my-4">
+
+        <div class="flex justify-between">
+            <img src="https://i.pravatar.cc/40" alt="" class="mr-2 rounded-full">
+            <button type="submit" class="p-2 text-white bg-blue-500 rounded-full">Tweet-a-roo!</button>
+        </div>
+    </form>
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border border-gray-300">
+    <div class="flex-shrink-0 mr-2">
+        <img src="https://i.pravatar.cc/50" alt="" class="mr-2 rounded-full">
+    </div>
+    <div>
+        <h5 class="mb-4 font-bold">Jonh Doe</h5>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vero repudiandae ea reiciendis ex
+            dolor illum odio fugiat fugit sapiente sit dolorem consequatur ullam nihil minus vel veniam,
+            ipsa voluptates.
+        </p>
+    </div>
+</div>
